52e32bae64c8592d3515e9eb6d10b7990bbee87e
Luis Felipe de Freitas <lluisfreitas2022gmail.com@Luiss-MBP.home>
2026-02-23 18:52:20 +0000

    ..

--- Module02/ex2/ft_custom_errors.py
+++ Module02/ex2/ft_custom_errors.py
@@ -1,36 +1,51 @@
 class GardenError(Exception):
-	pass
+    pass
 
 
 class PlantError(GardenError):
-	pass
+    pass
 
 
 class WaterError(GardenError):
-	pass
+    pass
 
 
-def garden_problem(flag: int) -> None:
-	if (flag == 1):
-		raise GardenError("Something wrong in the garden!\n")
+def plant_problem(flag: int) -> None:
+    if (flag == 1):
+        try:
+            raise PlantError("The tomato plant is wilting!")
+        except PlantError as e:
+            print(f"Caught PlantError: {e}")
 
 
-def plant_problem(flag: int) ->None:
-	if (flag == 1):
-		raise PlantError("The tomato plant is wilting!\n")
+def water_problem(flag: int) -> None:
+    if (flag == 1):
+        try:
+            raise WaterError("Not enough water in the tank!")
+        except WaterError as e:
+            print(f"Caught WaterError: {e}")
 
 
-def water_problem(flag: int):
-	if (flag == 1):
-		raise WaterError("Not enough water in the tank!\n")
-
-
-def test_errors(problens: list):
-	print("=== Custom Garden Errors Demo ===\n")
-	for problem in problens:
-		print(f"Testing {problem}...")
-		
-
+def test_errors() -> None:
+    print("=== Custom Garden Errors Demo ===\n")
+    print("Testing PlanError...")
+    try:
+        plant_problem(1)
+    except PlantError as e:
+        print(f"Caught a PlantError: {e}")
+    print("\nTesting WaterProblem...")
+    try:
+        water_problem(1)
+    except WaterError as e:
+        print(f"Caught a WaterError: {e}\n")
+    print("\nTesting catching all garden errors...")
+    for func in (plant_problem, water_problem):
+        try:
+            func(1)
+        except GardenError as e:
+            print(f"Caught a GardenError: {e}")
+    print("\nAll custum error types work correctly!")
 
 
 if __name__ == "__main__":
+    test_errors()
