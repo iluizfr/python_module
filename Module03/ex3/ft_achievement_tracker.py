1aac5af258428208dd1037d38c9af1465012e526
Luis Felipe de Freitas <lluisfreitas2022gmail.com@Luiss-MacBook-Pro.local>
2026-03-05 12:15:47 +0000

    ..

---
+++ Module03/ex3/ft_achievement_tracker.py
@@ -0,0 +1,52 @@
+class Player:
+    def __init__(self, name: str, achievements: set) -> None:
+        self.name = name
+        self.achievements = achievements
+
+    def get_info(self) -> None:
+        print(f"Player {self.name} achievements: {self.achievements}")
+
+
+if __name__ == "__main__":
+    print("=== Achievement Tracker System ===\n")
+
+    alice = Player("alice", {"first_kill", "level_10", "treasure_hunter",
+                   "speed_demon"})
+    bob = Player("bob", {'first_kill', 'level_10', 'boss_slayer', 'collector'})
+    charlie = Player("charlie", {'level_10', 'treasure_hunter', 'boss_slayer',
+                                 'speed_demon', 'perfectionist'})
+
+    alice.get_info()
+    bob.get_info()
+    charlie.get_info()
+
+    print("\n=== Achievement Analytics ===")
+    uniques = alice.achievements.union(bob.achievements, charlie.achievements)
+    print(f"All unique achievemets: {uniques}")
+    print(f"Total unique achievements: {len(uniques)}\n")
+
+    inter = alice.achievements.intersection(bob.achievements,
+                                            charlie.achievements)
+    print(f"Common to all players: {inter}")
+
+    rare_alice = alice.achievements.difference(
+        bob.achievements.union(charlie.achievements))
+
+    rare_bob = bob.achievements.difference(
+        alice.achievements.union(charlie.achievements))
+
+    rare_charlie = charlie.achievements.difference(
+        alice.achievements.union(bob.achievements))
+
+    rare = rare_alice | rare_bob | rare_charlie
+
+    print(f"Rare achievements (1 player): {rare}")
+
+    common_alice_bob = alice.achievements.intersection(bob.achievements)
+    print(f"\nAlice vc Bob common: {common_alice_bob}")
+
+    alice_unique = alice.achievements - bob.achievements
+    print(f"Alice unique: {alice_unique}")
+
+    bob_unique = bob.achievements - alice.achievements
+    print(f"Bob unique: {bob_unique}")
---
+++ Module03/ex4/ft_inventory_system.py
@@ -0,0 +1,108 @@
+import sys
+
+
+class NegativeError(Exception):
+    pass
+
+
+def parser(args: list) -> dict:
+    inventory = {}
+
+    for arg in args:
+        if ':' in arg:
+            key, value = arg.split(':', 1)
+            try:
+                tmp = int(value)
+                if tmp < 0:
+                    raise NegativeError("Cannot acept negative integers..")
+                inventory[key] = int(value)
+            except ValueError as erro_1:
+                print(f"Error: Only numbers for quantities: {erro_1}")
+            except NegativeError as erro_2:
+                print(f"Error: Only positive numbers: {erro_2}")
+    return inventory
+
+
+def calc_info(invetory: dict) -> None:
+    print("=== Inventory System Analysis ===")
+    list_values = inventory.values()
+    units = 0
+    for value in list_values:
+        units += value
+
+    print(f"Total items in inventory: {units}")
+    print("Unique item types:", len(inventory))
+
+    print("\n=== Current Inventory ===")
+    for key, value in inventory.items():
+        print(f"{key}: {value} units ({value / units * 100:.2f})")
+
+    print("\n=== Inventory Statistics ===")
+    bigger_value = 0
+    bigger_key = None
+    for key, value in inventory.items():
+        if value > bigger_value:
+            bigger_value = value
+            bigger_key = key
+    print(f"Most abundant: {bigger_key} ({bigger_value} units)")
+
+    smaller_value = bigger_value
+    smaller_key = None
+    for key, value in inventory.items():
+        if value < smaller_value:
+            smaller_value = value
+            smaller_key = key
+    print(f"Least abundant: {smaller_key} ({smaller_value} units)")
+
+    print("\n=== Item Categories ===")
+    moderate = {}
+    scarce = {}
+    for key, value in inventory.items():
+        if value > 3:
+            moderate[key] = value
+        else:
+            scarce[key] = value
+    print("Moderate:", moderate)
+    print("Scarce:", scarce)
+
+    print("\n=== Management Suggestions ===")
+    restock = []
+    for key, value in inventory.items():
+        if value < 2:
+            restock += [key]
+
+    if (len(restock) > 0):
+        i = 0
+        print("Restock needed:", end=" ")
+        for item in restock:
+            print(item, end="")
+            i += 1
+            if i < len(restock):
+                print(", ", end="")
+
+    print("\n\n=== Dictionary Properties Demo ===")
+    keys = inventory.keys()
+    values = inventory.values()
+    print("Dictionary keys:", end=" ")
+    j = 0
+    for key in keys:
+        print(key, end="")
+        j += 1
+        if j < len(keys):
+            print(", ", end="")
+
+    print("\nDictionary values:", end=" ")
+    j = 0
+    for value in values:
+        print(value, end="")
+        j += 1
+        if j < len(values):
+            print(", ", end="")
+
+    print(f"\nSample lookup - 'sword' in inventory: {'sword' in inventory}")
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    inventory = parser(args)
+    calc_info(inventory)
