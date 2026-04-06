683c64a696336b97bacb23629980615dcb1af8f6
Luis Felipe de Freitas <lluisfreitas2022gmail.com@Luiss-MacBook-Pro.local>
2026-04-06 13:54:49 +0100

    ..

--- Module07/ex1/ArtifactCard.py
+++ Module07/ex1/ArtifactCard.py
@@ -7,9 +7,22 @@ class ArtifactCard(Card):
         super().__init__(name, cost, rarity)
         self.durability = durability
         self.effect = effect
+        self.kind = "Artifact"
 
     def play(self, game_state: dict) -> dict:
-        pass
+        self.durability -= 1
+        return {
+            "card_played": self.name,
+            "mana_used": self.cost,
+            "effect": f"Permanent: {self.effect}"}
 
     def activate_ability(self) -> dict:
-        pass
+        if self.durability <= 0:
+            return {
+                "status": "destroyed",
+                "effect": "none"}
+        else:
+            return {
+                "artifact": f"{self.name}",
+                "effect": f"{self.effect}",
+                "durability_left": f"{self.durability}"}
--- Module07/ex1/Deck.py
+++ Module07/ex1/Deck.py
@@ -1,18 +1,51 @@
+import random
 from ex0.Card import Card
+from ex0.CreatureCard import CreatureCard
+from ex1.SpellCard import SpellCard
+from ArtifactCard import ArtifactCard
 
 
 class Deck:
+    cards: list[Card] = []
+
     def add_card(self, card: Card) -> None:
-        pass
+        self.cards.append(card)
 
     def remove_card(self, card_name: str) -> bool:
-        pass
+        for card in self.cards:
+            if card == card_name:
+                self.cards.remove(card_name)
+                return True
+        return False
 
     def shuffle(self) -> None:
-        pass
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)
 
     def draw_card(self) -> Card:
-        pass
+        if not self.cards:
+            raise ValueError("Deck is empty")
+
+        return self.cards.pop()
 
     def get_deck_stats(self) -> dict:
-        pass
+        creature = 0
+        spell = 0
+        artifact = 0
+        for card in self.cards:
+            if isinstance(card, CreatureCard):
+                creature += 1
+            elif isinstance(card, SpellCard):
+                spell += 1
+            elif isinstance(card, ArtifactCard):
+                artifact += 1
+
+        soma = creature + spell + artifact
+        avg = soma / 3
+
+        return {
+            "total_cards": len(self.cards),
+            "creatures": creature,
+            "spells": spell,
+            "artifacts": artifact,
+            "avg_cost": avg}
