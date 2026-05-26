b1e030616c04d6644a09a953277173aadc4bf475
Luis Felipe De Freitas <lluisfreitas2022@gmail.com>
2026-05-26 12:13:02 +0100

    ..

--- Module07/ex2/Strategy.py
+++ Module07/ex2/Strategy.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 from ex0.Creature import Creature
+from ex1.TransformCapability import TransformCapability
+from ex1. HealCapability import HealCapability
 
 
 class BattleStrategy(ABC):
@@ -13,36 +15,40 @@ class BattleStrategy(ABC):
 
 
 class NormalStrategy(BattleStrategy):
-    def act(self, creature) -> list[str]:
+    def act(self, creature: Creature) -> list[str]:
         if not self.is_valid(creature):
             message = "for this normal strategy"
             raise Exception(f"Invalid Creature {message}")
         return [creature.attack_target()]
 
-    def is_valid(self, creature) -> bool:
+    def is_valid(self, creature: Creature) -> bool:
         return hasattr(creature, "attack_target")
 
 
 class AggressiveStrategy(BattleStrategy):
-    def act(self, creature) -> list[str]:
+    def act(self, creature: Creature) -> list[str]:
         if not self.is_valid(creature):
             message = "for this aggressive strategy"
             raise Exception(f"Invalid Creature '{creature.name}' {message}")
-        return [creature.transform(),
-                creature.attack_target(),
-                creature.revert()]
+        if isinstance(creature, TransformCapability):
+            return [creature.transform(),
+                    creature.attack_target(),
+                    creature.revert()]
+        raise ValueError(f"Invalid Creature '{creature.name}' {message}")
 
-    def is_valid(self, creature) -> bool:
+    def is_valid(self, creature: Creature) -> bool:
         return hasattr(creature, "transform") and hasattr(creature, "revert")
 
 
 class DefensiveStrategy(BattleStrategy):
-    def act(self, creature) -> list[str]:
+    def act(self, creature: Creature) -> list[str]:
         if not self.is_valid(creature):
             message = "for this defensive strategy"
             raise Exception(f"Invalid Creature '{creature.name}' {message}")
-        return [creature.attack_target(),
-                creature.heal()]
+        if isinstance(creature, HealCapability):
+            return [creature.attack_target(),
+                    creature.heal()]
+        raise ValueError(f"Invalid Creature '{creature.name}' {message}")
 
-    def is_valid(self, creature) -> bool:
+    def is_valid(self, creature: Creature) -> bool:
         return hasattr(creature, "heal")
